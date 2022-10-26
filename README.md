62cfe4b81a6ac1f24a67ba35026fa52900b0c9e1
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-10-27 02:53:09 +0900

    Docs : Add readme

---
+++ README.md
@@ -0,0 +1,39 @@
+# Sentry Android Demo
+
+A simple demo project used sentry.
+
+Libraries used on the sample project
+------------------------------------
+* [Sentry](https://github.com/getsentry/sentry-java)
+* ViewBinding
+
+# Setup
+Set your DSN in `String.xml` file.
+```xml
+<resources>
+    <string name="sentry_dsn">YOUR_DSN</string>
+</resources>
+```
+
+# Preview
+![](https://user-images.githubusercontent.com/74607521/198098541-97c76988-68cc-43d9-9419-8daa0f3b4d7f.png)
+
+Developed By
+------------------------------------
+* NoKyungEun - <eunn.dev@gmail.com>
+
+License
+------------------------------------
+    Copyright 2022 NoKyungEun
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/kyungeun/sentrydemo/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.kyungeun.sentrydemo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.kyungeun.sentrydemo.databinding.ActivityMainBinding
+import io.sentry.Sentry
+import io.sentry.SentryLevel
+import io.sentry.protocol.User
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sendErrorBtn.setOnClickListener {
+            try {
+                throw Exception("Error Test")
+            } catch (e: Exception) {
+                Sentry.captureException(Exception("my error"))
+                Toast.makeText(this, "Completion!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.sendErrorTagBtn.setOnClickListener {
+            try {
+                throw Exception("Error Test")
+            } catch (e: Exception) {
+                Sentry.captureException(Exception("my error")) { scope ->
+                    scope.setTag("my-tag", "my value")
+                    scope.level = SentryLevel.WARNING
+                }
+                Toast.makeText(this, "Completion!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.messageBtn.setOnClickListener {
+            Sentry.captureMessage("Message Test")
+            Toast.makeText(this, "Completion!", Toast.LENGTH_SHORT).show()
+        }
+        binding.addUserBtn.setOnClickListener {
+            Sentry.configureScope { scope ->
+                scope.setTag("user", "login")
+                scope.level = SentryLevel.DEBUG
+                scope.user = User().apply {
+                    id = "123456789"
+                    username = "demo"
+                    email = "demo@example.com"
+                }
+            }
+        }
+        binding.clearUserBtn.setOnClickListener {
+            Sentry.configureScope { scope ->
+                scope.setTag("user", "logout")
+                scope.user = null
+            }
+        }
+    }
+}
